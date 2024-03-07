7781a3fc4b0975acb965717a9d6bcc0f51e58043
Nicholas Q <nickquam38@gmail.com>
2024-03-07 15:44:56 -0700

    Fixed nested project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,53 @@
+<script>
+	import Header from './Header.svelte';
+	import './styles.css';
+</script>
+
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:400,700&display=swap">
+
+<div class="app">
+	<Header />
+
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+        <p>created by <strong>Qick</strong></p> 
+	</footer>
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+		font-family: 'Lora', serif;
+
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding-top: 10rem;
+		width: 100%;
+		max-width: 64rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 12px;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,51 @@
+<script>
+
+</script>
+
+<svelte:head>
+    <title>TraceValley</title>
+    <meta name="description" content="Innovative Technology Solutions by TraceValley" />
+</svelte:head>
+
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora,700&display=swap">
+
+<main>
+
+    <section class="hero">
+        <h1>tracevalley</h1>
+        <p>welcome to the future</p>
+    </section>
+
+	  
+
+    <!-- Add your other sections like About, Services, Contact here -->
+
+</main>
+
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+		margin: 0;
+	}
+
+	h1 {
+		width: 100%;
+		font-weight: bold;
+		font-size: 5rem;
+		margin: 0;
+
+	}
+
+	p{
+		font-style: italic;
+		font-size: 1.3rem;
+		margin: 0;
+	}
+
+
+</style>
